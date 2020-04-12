04dcd4eed53df97fd1c99616af21b36997715264
thinhnv <maxiqboy@gmail.com>
2020-04-12 09:47:50 +0700

    step 2 - breaking the app into components

---
+++ src/app/components/CalendarWeek.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="calendar-week" class="container">
+    <div class="columns is-mobile">
+      <CalendarDay
+        v-for="day in sharedState.seedData"
+        :key="day.id"
+        :day="day"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { store } from "../store";
+import CalendarDay from "./CalendarDay";
+
+export default {
+  name: "CalendarWeek",
+  components: {
+    CalendarDay,
+  },
+  data() {
+    return {
+      sharedState: store.state,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#calendar-week {
+  margin-bottom: 50px;
+  .column {
+    padding: 0 0 0 0;
+  }
+}
+</style>
